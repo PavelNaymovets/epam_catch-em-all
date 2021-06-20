7958e6fe8830d93a09f16486458f4394e2fab5a5
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-06-20 13:36:57 +0300

    Answer

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -2,27 +2,41 @@ package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class CatchEmAll {
 
     //You may set another exception in this field;
-    static Exception exception = new FileNotFoundException();
+    static Exception exception = new IndexOutOfBoundsException("ioobe");
+
+    public static void main(String[] args) throws Exception {
+        try {
+            try {
+                riskyMethod();
+            } catch(FileNotFoundException fileNotFoundException){
+                throw new IllegalArgumentException("Resource is missing", fileNotFoundException);
+            } catch(ArithmeticException | NumberFormatException total){
+                System.err.println(total.getMessage());
+            }
+        } catch (IOException ioException){
+            throw new IllegalArgumentException("Resource error", ioException);
+        }
+    }
 
     public static void riskyMethod() throws Exception {
         throw exception;
     }
-
-    public static void main(String[] args) throws Exception {
-        try{
-            riskyMethod();
-        } catch(Exception e){
-            if(e instanceof FileNotFoundException){
-                throw new IllegalArgumentException("Resource is missing");
-            } else if((e instanceof ArithmeticException) || (e instanceof NumberFormatException)){
-                System.setErr(System.err);
-            } else if(e instanceof IOException){
-                throw new IllegalArgumentException("Resource error");
-            }
-        }
-    }
 }
+
+/*
+try{
+        riskyMethod();
+        } catch(Exception e){
+        if(e instanceof FileNotFoundException){
+        throw new IllegalArgumentException("Resource is missing");
+        } else if((e instanceof ArithmeticException) || (e instanceof NumberFormatException)){
+        System.setErr(System.err);
+        } else if(e instanceof IOException){
+        throw new IllegalArgumentException("Resource error");
+        }
+        }*/
